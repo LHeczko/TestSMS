16f31cba4f53623178ce35fcb604b8c1736ddd67
LHeczko <lheczko@gmail.com>
2019-01-08 11:40:17 +0100

    TestSMS

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication15.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet()]
+        public OkResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("SendSMS")]
+        public OkObjectResult SendSMS()
+        {
+            System.Net.WebRequest vZadanie;
+            Byte[] vTablicaZnakow;
+
+            String wynik="";
+
+            try
+            {
+                System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Tls11;
+
+                vZadanie = System.Net.WebRequest.Create("https://api.smslabs.net.pl/apiSms/sendSms");
+                vZadanie.Headers.Add("Authorization", "Basic NjYxNGY4ZTYwNDU0YTdmMmNjMjliODNjNmE3NWVkOTYzOWQxYWZhZTpmOTBkMmZjMzgxZGJmMmI2NTYzZWNlOTAzZDA2MjExYmI3NDc5YzA2");
+                vZadanie.Timeout = 1200000;
+                vZadanie.Method = "PUT";
+                vZadanie.ContentType = "application/x-www-form-urlencoded";
+
+                vTablicaZnakow = System.Text.Encoding.UTF8.GetBytes("flash=0&expiration=0&phone_number=%2B48601172078&sender_id=SMS%20TEST&message=Test");
+
+                vZadanie.ContentLength = vTablicaZnakow.Length;
+
+                System.IO.Stream vStrumien2 = vZadanie.GetRequestStream();
+                vStrumien2.Write(vTablicaZnakow, 0, vTablicaZnakow.Length);
+                vStrumien2.Close();
+
+                using (var vOdpowiedz = vZadanie.GetResponse())
+                {
+                    using (var vStrumien = vOdpowiedz.GetResponseStream())
+                    {
+                        using (var ms = new System.IO.MemoryStream())
+                        {
+                            vStrumien.CopyTo(ms);
+                            ms.Position = 0;
+
+                            using (var vCzytnik = new System.IO.StreamReader(ms))
+                                wynik = vCzytnik.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                wynik = e.Message;
+            }
+            return Ok(wynik);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace WebApplication15
+{
+    public class RouteConvention : IApplicationModelConvention
+    {
+        private readonly AttributeRouteModel _centralPrefix;
+
+        public RouteConvention(IRouteTemplateProvider routeTemplateProvider)
+        {
+            _centralPrefix = new AttributeRouteModel(routeTemplateProvider);
+        }
+
+        public void Apply(ApplicationModel application)
+        {
+            foreach (var controller in application.Controllers)
+            {
+                var matchedSelectors = controller.Selectors.Where(x => x.AttributeRouteModel != null).ToList();
+                if (matchedSelectors.Any())
+                {
+                    foreach (var selectorModel in matchedSelectors)
+                    {
+                        selectorModel.AttributeRouteModel = AttributeRouteModel.CombineAttributeRouteModel(_centralPrefix,
+                            selectorModel.AttributeRouteModel);
+                    }
+                }
+
+                var unmatchedSelectors = controller.Selectors.Where(x => x.AttributeRouteModel == null).ToList();
+                if (unmatchedSelectors.Any())
+                {
+                    foreach (var selectorModel in unmatchedSelectors)
+                    {
+                        selectorModel.AttributeRouteModel = _centralPrefix;
+                    }
+                }
+            }
+        }
+    }
+    public static class MvcOptionsExtensions
+    {
+        public static void UseCentralRoutePrefix(this MvcOptions opts, IRouteTemplateProvider routeAttribute)
+        {
+            opts.Conventions.Insert(0, new RouteConvention(routeAttribute));
+        }
+    }
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(opt =>
+            {
+                opt.UseCentralRoutePrefix(new RouteAttribute("TestSMS"));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
